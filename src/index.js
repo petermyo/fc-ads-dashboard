58a3e1ce9a4ada0ef63e1385eb26e7cd43400a12
petermyo <p3t3rmyo@gmail.com>
2025-06-16 01:30:13 +0630

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Assuming you might have a global CSS file, create if not present
 import App from './App'; // Import your main App component
-import reportWebVitals from './reportWebVitals'; // Optional: for performance measurement
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +10,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals was removed as it's optional and not required for core functionality.
+// If you want to add performance measurement later, you can re-implement it.
